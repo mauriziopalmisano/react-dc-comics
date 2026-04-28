0b8b1d113d83e102caab6001c2f244990f595aa5
mauriziopalmisano <maurizio.palmisano1@gmail.com>
2026-04-28 19:08:59 +0200

    completamento delle ultime parti del footer : -info list -social link

--- src/components/FooterInfoList.jsx
+++ src/components/FooterInfoList.jsx
@@ -1,44 +1,52 @@
 function FooterInfoList() {
+    const footerInfoListEl = [
+        {
+            id: 1,
+            category: 'DC comics',
+            links: [
+                { id: 1, title: 'Characters' }, { id: 2, title: 'Comics' }, { id: 3, title: 'Movies' }, { id: 4, title: 'TV' }, { id: 5, title: 'Games' }, { id: 6, title: 'Videos' }, { id: 7, title: 'News' }
+            ]
+        },
+        {
+            id: 2,
+            category: 'Shop',
+            links: [
+                { id: 1, title: 'Shop DC' }, { id: 2, title: 'Shop DC Collectibles' }
+            ]
+        },
+        {
+            id: 3,
+            category: 'DC',
+            links: [
+                { id: 1, title: 'Terms Of Use' }, { id: 2, title: 'Privacy policy (New)' }, { id: 3, title: 'Ad Choices' }, { id: 4, title: 'Advertising' }, { id: 5, title: 'Jobs' }, { id: 6, title: 'Subscriptions' }, { id: 7, title: 'Talent Workshops' }, { id: 8, title: 'CPSC Certificates' }, { id: 9, title: 'Ratings' }, { id: 10, title: 'Shop Help' }, { id: 11, title: 'Contact Us' }
+            ]
+        },
+        {
+            id: 4,
+            category: 'Sites',
+            links: [
+                { id: 1, title: 'DC' }, { id: 2, title: 'MAD Magazine' }, { id: 3, title: 'DC Kids' }, { id: 4, title: 'DC Universe' }, { id: 5, title: 'DC Power Visa' }
+            ]
+        },
+    ];
+
+    const infoListJSX = footerInfoListEl.map(element => {
+        const { id, category, links } = element;
+        return <ul key={id} className="text-white list-unstyled px-2 mx-3">
+            <h4>{category.toUpperCase()}</h4>
+            {links.map(link => {
+                const { id, title } = link;
+                return <li key={id}>
+                    <a href="#" className="text-decoration-none text-white-50">{title}</a>
+                </li>
+            })}
+        </ul>
+    })
+
     return <>
-    <div className="col">
-        <ul>
-            <h4>DC COMICS</h4> 
-            <li>Characters</li>
-            <li>Comics</li>
-            <li>Movies</li>
-            <li>TV</li>
-            <li>Games</li>
-            <li>Videos</li>
-            <li>News</li>
-        </ul>
-        <ul>
-            <h4>SHOP</h4> 
-            <li>Shop DC</li>
-            <li>Shop DC Collectibles</li>
-        </ul>
-        <ul>
-            <h4>DC</h4> 
-            <li>Terms Of Use</li>
-            <li>Privacy policy (New)</li>
-            <li>Ad Choices</li>
-            <li>Advertising</li>
-            <li>Jobs</li>
-            <li>Subscriptions</li>
-            <li>Talent Workshops</li>
-            <li>CPSC Certificates</li>
-            <li>Ratings</li>
-            <li>Shop Help</li>
-            <li>Contact Us</li>
-        </ul>
-        <ul>
-            <h4>SITES</h4> 
-            <li>DC</li>
-            <li>MAD Magazine</li>
-            <li>DC Kids</li>
-            <li>DC Universe</li>
-            <li>DC Power Visa</li>
-        </ul>
-    </div>
+        <div className="col d-flex flex-wrap flex-column align-content-start info-list ">
+            {infoListJSX}
+        </div>
     </>
 }
 
--- src/components/FooterSocials.jsx
+++ src/components/FooterSocials.jsx
@@ -1,26 +1,43 @@
-function FooterSocials (){
+function FooterSocials() {
+    const socialsEl = [
+        {
+            id: 1,
+            url: '/img/footer-facebook.png'
+        },
+        {
+            id: 2,
+            url: 'img/footer-twitter.png'
+        },
+        {
+            id: 1,
+            url: '/img/footer-youtube.png'
+        },
+        {
+            id: 1,
+            url: '/img/footer-pinterest.png'
+        },
+        {
+            id: 1,
+            url: '/img/footer-periscope.png'
+        }
+    ]
+
+    const socialsJSX = socialsEl.map(element => {
+        const {id, url} = element;
+        return <a key={id} href="#">
+                    <img src={url} alt="" />
+                </a>
+    })
+
+
     return <>
-        <div className="col-12 d-flex">
+        <div className="col-12 d-flex justify-content-between footer-socials bg-dark align-items-center px-5">
             <div>
-            <button>SIGN-UP NOW!</button>
+                <button className="btn btn-outline-primary rounded-0 text-white border-3 fs-5 p-3 fw-bold">SIGN-UP NOW!</button>
             </div>
-            <div>
-                <button>FOLLOW US</button>
-                <a href="#">
-                    <img src="/img/footer-facebook.png" alt="" />
-                </a>
-                <a href="#">
-                    <img src="/img/footer-twitter.png" alt="" />
-                </a>
-                <a href="#">
-                    <img src="/img/footer-youtube.png" alt="" />
-                </a>
-                <a href="#">
-                    <img src="/img/footer-pinterest.png" alt="" />
-                </a>
-                <a href="#">
-                    <img src="/img/footer-periscope.png" alt="" />
-                </a>
+            <div className="d-flex justify-content-evenly  align-items-center gap-3">
+                <span className="text-primary fs-3 fw-bold">FOLLOW US</span>
+                {socialsJSX}
             </div>
         </div>
     </>
